26fc4335d2c031028e80dc1e90b3fed5d9f43fec
awbridgers <awbridgers@users.noreply.github.com>
2020-03-16 16:51:58 -0400

    Updates

---
+++ precache-manifest.509fa27761caa4d0e4c51d5dbb90ab03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a122fdbb5582055575359ba9ec7fcf4",
+    "url": "/bsd-bracket-challenge/index.html"
+  },
+  {
+    "revision": "7ff95fed96f7aac3e32f",
+    "url": "/bsd-bracket-challenge/static/css/main.8c1ede97.chunk.css"
+  },
+  {
+    "revision": "0991d764ee08f43e21e2",
+    "url": "/bsd-bracket-challenge/static/js/2.8b15f367.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/bsd-bracket-challenge/static/js/2.8b15f367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff95fed96f7aac3e32f",
+    "url": "/bsd-bracket-challenge/static/js/main.7e14cd3a.chunk.js"
+  },
+  {
+    "revision": "3a47513cf77ba46f501e",
+    "url": "/bsd-bracket-challenge/static/js/runtime-main.ea46dbdc.js"
+  }
+]);
